e96077bed3a51da93ee29ceca1c6654d46f2844c
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-02-09 20:12:41 -0300

    filtrando questao por id

--- app/api/questoes/[id]/route.js
+++ app/api/questoes/[id]/route.js
@@ -1,6 +1,10 @@
 import { NextResponse } from "next/server";
 import questoes from "@/data/bancoDeQuestoes";
 
-export function GET(request, params) {
-  return NextResponse.json(questoes[0].converterParaObjeto());
+export function GET(request, { params }) {
+  const id = +params.id;
+  const questoesFiltradas = questoes.filter((questao) => questao.id === id);
+  if (questoesFiltradas.length === 1)
+    return NextResponse.json(questoesFiltradas[0].converterParaObjeto());
+  return NextResponse.json([]);
 }
