b575e604b03418f6f8325f3b5839189c74190ea0
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-02-23 18:58:34 -0300

    buscando valores da api

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import Questionario from "@/components/Questionario";
 import QuestaoModel from "@/model/questao";
 import RespostaModel from "@/model/resposta";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const questaoMock = new QuestaoModel(1, "Melhor cor?", [
   RespostaModel.errada("Verde"),
@@ -10,29 +10,43 @@ const questaoMock = new QuestaoModel(1, "Melhor cor?", [
   RespostaModel.errada("Azul"),
   RespostaModel.certa("Preta"),
 ]);
+
+const BASE_URL = "http://localhost:3000/api";
+
 export default function Home() {
+  const [idsDasQuestoes, setIdsDasQuestoes] = useState<number[]>([]);
   const [questao, setQuestao] = useState(questaoMock);
+
+  async function carregarIdsDasQuestoes() {
+    const resposta = await fetch(`${BASE_URL}/questionario`);
+    const idsDasQuestoes = await resposta.json();
+    setIdsDasQuestoes(idsDasQuestoes);
+  }
+
+  async function carregarQuestao(idQuestao: number) {
+    const resposta = await fetch(`${BASE_URL}/questoes/${idQuestao}`);
+    const json = await resposta.json();
+    console.log(json)
+  }
+
+  useEffect(() => {
+    carregarIdsDasQuestoes();
+  }, []);
+
+  useEffect(() => {
+    idsDasQuestoes.length > 0 && carregarQuestao(idsDasQuestoes[0])
+  }, [idsDasQuestoes]);
 
   function questaoRespondida(questao: QuestaoModel) {}
 
   function irParaProximoPasso() {}
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-      }}
-    >
-      <Questionario
-        questao={questao}
-        ultima={true}
-        questaoRespondida={questaoRespondida}
-        irParaProximoPasso={irParaProximoPasso}
-      />
-    </div>
+    <Questionario
+      questao={questao}
+      ultima={true}
+      questaoRespondida={questaoRespondida}
+      irParaProximoPasso={irParaProximoPasso}
+    />
   );
 }
