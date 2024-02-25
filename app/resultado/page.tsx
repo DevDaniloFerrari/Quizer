453f64de3880ef3fb32d7978964bb9f76ee17763
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-02-25 12:54:23 -0300

    melhorando pagina de resultado

--- app/resultado/page.tsx
+++ app/resultado/page.tsx
@@ -1,7 +1,20 @@
-export default function resultado() {
+"use client";
+import styles from '@/styles/Resultado.module.css'
+
+export default function resultado({
+  searchParams,
+}: {
+  searchParams: { total: number; certas: number };
+}) {
+  const { total, certas } = searchParams;
+  const percentual = Math.round((certas / total) * 100);
+
   return (
-    <div>
-      <h1>Resultado</h1>
+    <div className={styles.resultado}>
+      <h1>Resultado Final</h1>
+      <div>{total}</div>
+      <div>{certas}</div>
+      <div>{`${percentual}%`}</div>
     </div>
   );
 }
