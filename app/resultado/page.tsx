607b277d19a8be083c79a8142e9ee75e6c36d203
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-02-25 13:08:17 -0300

    implementando componente estatistica

--- app/resultado/page.tsx
+++ app/resultado/page.tsx
@@ -1,5 +1,7 @@
 "use client";
-import styles from '@/styles/Resultado.module.css'
+import Botao from "@/components/Botao";
+import Estatistica from "@/components/Estatistica";
+import styles from "@/styles/Resultado.module.css";
 
 export default function resultado({
   searchParams,
@@ -12,9 +14,12 @@ export default function resultado({
   return (
     <div className={styles.resultado}>
       <h1>Resultado Final</h1>
-      <div>{total}</div>
-      <div>{certas}</div>
-      <div>{`${percentual}%`}</div>
+      <div style={{ display: "flex" }}>
+        <Estatistica texto="Perguntas" valor={total} />
+        <Estatistica texto="Certas" valor={certas} corFundo="#9CD2A4"/>
+        <Estatistica texto="Percentual" valor={`${percentual}%`} corFundo="#DE6A33"/>
+      </div>
+      <Botao href="/" texto="Tentar Novamente"/>
     </div>
   );
 }
