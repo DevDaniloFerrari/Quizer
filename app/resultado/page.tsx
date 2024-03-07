6fdf5ae8614637ae3ef1db24892ee43475a1ae6c
CaioMassola <caio.massola@outlook.com>
2024-03-07 17:19:14 -0300

    feature: Implementar botão de download de resultado #9

--- app/resultado/page.tsx
+++ app/resultado/page.tsx
@@ -1,16 +1,34 @@
 "use client";
+import { useCallback } from "react";
+import html2canvas from "html2canvas";
 import Botao from "@/components/Botao";
 import Estatistica from "@/components/Estatistica";
 import IconeGithub from "@/components/IconeGithub";
 import styles from "@/styles/Resultado.module.css";
 
-export default function resultado({
+export default function Resultado({
   searchParams,
 }: {
   searchParams: { total: number; certas: number };
 }) {
   const { total, certas } = searchParams;
   const percentual = Math.round((certas / total) * 100);
+
+  const capturarScreenshot = useCallback(async () => {
+    try {
+      const canvas = await html2canvas(document.body),
+        imageData = canvas.toDataURL("image/jpeg"),
+        downloadLink = document.createElement("a");
+
+      Object.assign(downloadLink, {
+        href: imageData,
+        download: "screenshot.jpg"
+      }).click();
+
+    } catch {
+      console.error("Erro ao obter screenshot");
+    }
+  }, []);
 
   return (
     <div className={styles.resultado}>
@@ -25,6 +43,7 @@ export default function resultado({
         />
       </div>
       <Botao href="/" texto="Tentar Novamente" />
+      <Botao texto="Baixar Resultado" onClick={capturarScreenshot} />
       <IconeGithub />
     </div>
   );
