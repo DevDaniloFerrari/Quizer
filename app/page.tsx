4df7c05eae64a82db4e001f4774163611b4acad4
CaioMassola <caio.massola@outlook.com>
2024-03-09 11:22:46 -0300

    feature: Deixar o tempo de resposta configurável #2

--- app/page.tsx
+++ app/page.tsx
@@ -6,24 +6,38 @@ import { useState } from "react";
 
 export default function Home() {
   const [quantidadePerguntas, setQuantidadePerguntas] = useState(10);
+  const [duracaoPerguntas, setDuracaoPerguntas] = useState(10);
 
   return (
     <div className={styles.home}>
       <h1>Quizer</h1>
-      <label className={styles.input}>
-        Quantidade de Perguntas:
-        <input
-          className={styles.quantidade}
-          type="number"
-          min={1}
-          max={30}
-          value={quantidadePerguntas}
-          onChange={(event) => setQuantidadePerguntas(+event.target.value)}
-        />
-      </label>
+      <div className={styles.separador}>
+        <label className={styles.input}>
+          Quantidade de Perguntas:
+          <input
+            className={styles.quantidade}
+            type="number"
+            min={1}
+            max={30}
+            value={quantidadePerguntas}
+            onChange={(event) => setQuantidadePerguntas(+event.target.value)}
+          />
+        </label>
+        <label className={styles.input}>
+          Duração das questões:
+          <input
+            className={styles.quantidade}
+            type="number"
+            min={10}
+            max={60}
+            value={duracaoPerguntas}
+            onChange={(event) => setDuracaoPerguntas(+event.target.value)}
+          />
+        </label>
+      </div>
       <Botao
         texto="Iniciar"
-        href={`/perguntas?quantidadePerguntas=${quantidadePerguntas}`}
+        href={`/perguntas?quantidadePerguntas=${quantidadePerguntas}&duracaoPerguntas=${duracaoPerguntas}`}
       />
       <IconeGithub />
     </div>
