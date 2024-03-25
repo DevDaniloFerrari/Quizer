f9b084507a4bbfa6b04737434ebb4afa90f1bb4f
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-03-25 18:16:06 -0300

    salvando resultado

--- app/resultado/page.tsx
+++ app/resultado/page.tsx
@@ -1,10 +1,22 @@
 "use client";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import html2canvas from "html2canvas";
 import Botao from "@/components/Botao";
 import Estatistica from "@/components/Estatistica";
 import IconeGithub from "@/components/IconeGithub";
 import styles from "@/styles/Resultado.module.css";
+import { navegarPorLink } from "@/functions/utils";
+import { Classificacao, QuizRespondido } from "@/model/classificacao";
+import useAuth from "@/data/hook/useAuth";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  query,
+  where,
+  deleteDoc,
+} from "firebase/firestore";
+import { db } from "@/firebase";
 
 export default function Resultado({
   searchParams,
@@ -14,6 +26,47 @@ export default function Resultado({
   const { total, certas } = searchParams;
   const percentual = Math.round((certas / total) * 100);
   const [loading, setLoading] = useState(false);
+  const [loadingJogarNovamente, setLoadingJogarNovamente] = useState(false);
+  const { usuario } = useAuth();
+
+  const jogarNovamente = () => {
+    setLoadingJogarNovamente(true);
+    navegarPorLink("/");
+  };
+
+  async function computarQuizRespondido() {
+    const querySnapshot = await getDocs(
+      query(collection(db, "classificacao"), where("uid", "==", usuario.uid))
+    );
+    const classificacao = querySnapshot.docs.map((doc) =>
+      Classificacao.criarUsandoObjeto(JSON.parse(JSON.stringify(doc.data())))
+    )[0];
+
+    const quizRespondido = new QuizRespondido(certas, total - certas);
+
+    if (!classificacao) {
+      let primeiraClassificao = new Classificacao(usuario.uid, [
+        quizRespondido,
+      ]);
+      await addDoc(
+        collection(db, "classificacao"),
+        JSON.parse(JSON.stringify(primeiraClassificao))
+      );
+      return;
+    }
+
+    classificacao.adicionarQuizResponido(quizRespondido);
+    const docRef = querySnapshot.docs[0].ref;
+    await deleteDoc(docRef);
+    await addDoc(
+      collection(db, "classificacao"),
+      Object.assign({}, classificacao)
+    );
+  }
+
+  useEffect(() => {
+    if (usuario.uid) computarQuizRespondido();
+  }, []);
 
   const capturarScreenshot = useCallback(async () => {
     try {
@@ -24,13 +77,11 @@ export default function Resultado({
 
       Object.assign(downloadLink, {
         href: imageData,
-        download: "screenshot.jpg"
+        download: "screenshot.jpg",
       }).click();
-
     } catch {
       console.error("Erro ao obter screenshot");
-    }
-    finally {
+    } finally {
       setLoading(false);
     }
   }, []);
@@ -47,8 +98,16 @@ export default function Resultado({
           corFundo="#DE6A33"
         />
       </div>
-      <Botao texto="Tentar Novamente" href="/"/>
-      <Botao texto="Baixar Resultado" onClick={capturarScreenshot} loading={loading} />
+      <Botao
+        texto="Tentar Novamente"
+        onClick={jogarNovamente}
+        loading={loadingJogarNovamente}
+      />
+      <Botao
+        texto="Baixar Resultado"
+        onClick={capturarScreenshot}
+        loading={loading}
+      />
       <IconeGithub />
     </div>
   );
