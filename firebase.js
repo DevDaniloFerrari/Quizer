54081319501cbfb7f887802b82acbbdc1d0d3e79
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2024-04-04 10:57:46 -0300

    configurando api de desenvolvimento

--- firebase.js
+++ firebase.js
@@ -9,7 +9,7 @@ import {
 } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AMBIENTE_DE_DESENVOLVIMENTO",
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
